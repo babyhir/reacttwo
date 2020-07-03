921f9696231f7c40001d481e0fc0bc431183d1cf
babyhir <loyal_mav89@hotmail.com>
2020-07-04 01:55:14 +0800

    First commit to reacttwo

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer(){
+    return(
+        <header className="bg-gray-200 text-center">
+            &copy; Copyright 2020
+        </header>
+    )
+}
+
+export default Footer
+
---
+++ src/Views/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import HelloWorld from '../Components/HelloWorld'
+
+function Home(){
+    return(
+        <div>
+             <h1 className="font-bold text-2xl">This is the Home Page</h1>
+            <HelloWorld name="Mee2" />
+        </div>
+
+    )
+}
+
+export default Home
